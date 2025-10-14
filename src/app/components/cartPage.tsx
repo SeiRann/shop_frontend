1e5d5d17c3a71bea1e23fe628c7fb1a25dacd357
Seiran <bo646ru@gmail.com>
2025-10-14 15:26:27 -0400

    made cart work with the amounts of products

--- src/app/components/cartPage.tsx
+++ src/app/components/cartPage.tsx
@@ -2,14 +2,46 @@
 import { useEffect, useState } from "react";
 import { useGlobalContext } from "../context/globalContext";
 import ProductListView from "./productListView";
+import { IProduct } from "./productViewCard";
+
+interface CartItem {
+    product: IProduct;
+    product_amount: number;
+}
 
 export default function CartPage() {
-    const { cart, fetchCartProducts } = useGlobalContext();
-    const [products, setProducts] = useState<{}>();
+    const { fetchCartProducts, cart } = useGlobalContext();
+    const [products, setProducts] = useState<CartItem[]>([]);
 
     useEffect(() => {
-        console.log(fetchCartProducts());
+        const loadProducts = async () => {
+            const result = await fetchCartProducts();
+            console.log("Fetched products:", result);
+
+            const productsArray: CartItem[] = Object.values(result).map(
+                (cartItem: any) => ({
+                    product: cartItem.product ?? cartItem, // handle different backend shapes
+                    product_amount: cart.has(cartItem.product_id)
+                        ? cart.get(cartItem.product_id)
+                        : 0,
+                }),
+            );
+
+            setProducts(productsArray);
+        };
+
+        loadProducts();
     }, []);
 
-    return <div></div>;
+    return (
+        <div>
+            {products.map((cartItem) => (
+                <ProductListView
+                    key={cartItem.product.product_id}
+                    product={cartItem.product}
+                    product_amount={cartItem.product_amount}
+                />
+            ))}
+        </div>
+    );
 }
--- src/app/components/productListView.tsx
+++ src/app/components/productListView.tsx
@@ -2,13 +2,14 @@ import { IProduct } from "./productViewCard";
 import Image from "next/image";
 
 interface IProductListViewProps {
+    key: string;
     product: IProduct;
     product_amount: number;
 }
 
 export default function ProductListView(props: IProductListViewProps) {
     return (
-        <div className="flex justify-between">
+        <div className="flex justify-between" key={props.key}>
             <Image width={200} height={200} src={props.product.image} alt="" />
             <div className="flex flex-col gap-0.5">
                 <p>{props.product.title}</p>
